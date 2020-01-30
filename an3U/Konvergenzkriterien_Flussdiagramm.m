6b0c59a12cd7e31f25a6471cee1f3b7798190bb4
David Spörri <spoerridavid@gmail.com>
2020-01-30 15:12:03 +0100

    add tab spacing

--- an3U/Konvergenzkriterien_Flussdiagramm.m
+++ an3U/Konvergenzkriterien_Flussdiagramm.m
@@ -1,4 +1,3 @@
-
 %Konvergenzkriterien gemäss Flussdiagramm
 
 clear all
@@ -23,24 +22,25 @@ W=simplify(limit((abs(f(n))^(1/n)),n,inf))
 fn(n)=diff(f(n),n,1);
 
 if D~=0
-disp("divergent gemäss Divergenzkriterium")
+    disp("divergent gemäss Divergenzkriterium")
 elseif D==0
-if W<1||Q<1
-disp("konvergent gemäss W/Q-Kriterium")
-elseif W>1||Q>1
-disp("divergent gemäss W/Q-Kriterium")
-elseif W==1||Q==1
-if f(n0)>0 && fn(n0)<0
-I(n)=simplify(int(f(n),n0,inf))
-if I(n0)==inf
-disp("divergent gemäss Integralkriterium")
-elseif I(n0)<inf
-disp("konvergent gemäss Integralkriterium")
-else
-disp("Selber ausrechnen!")
+    if W<1||Q<1
+        disp("konvergent gemäss W/Q-Kriterium")
+    elseif W>1||Q>1
+        disp("divergent gemäss W/Q-Kriterium")
+    elseif W==1||Q==1
+        if f(n0)>0 && fn(n0)<0
+            I(n)=simplify(int(f(n),n0,inf))
+            if I(n0)==inf
+                disp("divergent gemäss Integralkriterium")
+            elseif I(n0)<inf
+                disp("konvergent gemäss Integralkriterium")
+            else
+                disp("Selber ausrechnen!")
+            end
+        else
+            disp("Selber ausrechnen!")
+        end
+    end
 end
-else
-disp("Selber ausrechnen!")
-end
-end
-end
+
