291d84f3aa574ef8b0bc93e0cd6c46aac118c03e
David Spörri <spoerridavid@gmail.com>
2020-10-21 15:42:17 +0200

    move functions to separate file

---
+++ thma/functions.py
@@ -0,0 +1,40 @@
+import CoolProp.CoolProp as CP
+
+def get_property(property: str, state: dict, fluid: str):
+    arguments = map_properties(state)
+    #print(arguments)
+
+    value = CP.PropsSI(property, arguments[0], arguments[1], arguments[2], arguments[3], fluid)
+    message = property + ' = ' + str(value) + ' for "' + fluid + '" at '
+    for key in state.keys():
+        message += key + '=' + str(state[key]) + '; '
+    print(message)
+
+    return value
+
+
+def get_phase(state: dict, fluid):
+    arguments = map_properties(state)
+    return CP.PhaseSI(arguments[0], arguments[1], arguments[2], arguments[3], fluid)
+
+
+def complete_state(state: dict, fluid: str):
+    required_properties = ['T', 'P', 'H', 'S']
+    for property in required_properties:
+        if not property in state:
+            state[property] = get_property(property, state, fluid)
+
+    phase = get_phase(state, fluid)
+    state['phase'] = phase
+    if (phase == 'twophase'):
+        state['Q'] = get_property('Q', state, fluid)
+
+
+def map_properties(properties: dict):
+    arguments = []
+
+    for key in properties.keys():
+        arguments.append(key)
+        arguments.append(properties[key])
+
+    return arguments
--- thma/orc.py
+++ thma/orc.py
@@ -1,8 +1,8 @@
-import CoolProp.CoolProp as CP
+from functions import complete_state
 
 fluid = 'Isopentane'
 
-tk = 50+273
+tk = 40+273
 tv = 150+273
 
 power_vaporizer = 650e3
@@ -10,7 +10,7 @@ eta_turbine = 0.85
 eta_regenerator = 0.75
 q_turbine_entry = 1
 
-states = [None] * 7
+states = [dict()] * 7
 for key, value in enumerate(states):
     states[key] = dict()
 
@@ -18,43 +18,6 @@ states[4]['T'] = tv
 states[4]['Q'] = q_turbine_entry
 states[1]['T'] = tk
 states[1]['Q'] = 0
-
-def get_property(property: str, state: dict, fluid: str):
-    value = None
-    arguments = map_properties(state)
-    #print(arguments)
-
-    value = CP.PropsSI(property, arguments[0], arguments[1], arguments[2], arguments[3], fluid)
-    message = property + ' = ' + str(value) + ' for "' + fluid + '" at '
-    for key in state.keys():
-        message += key + '=' + str(state[key]) + '; '
-    print(message)
-
-    return value
-
-def get_phase(state: dict, fluid):
-    arguments = map_properties(state)
-    return CP.PhaseSI(arguments[0], arguments[1], arguments[2], arguments[3], fluid)
-
-def complete_state(state: dict, fluid: str):
-    required_properties = ['T', 'P', 'H', 'S']
-    for property in required_properties:
-        if not property in state:
-            state[property] = get_property(property, state, fluid)
-
-    phase = get_phase(state, fluid)
-    state['phase'] = phase
-    if (phase == 'twophase'):
-        state['Q'] = get_property('Q', state, fluid)
-
-def map_properties(properties: dict):
-    arguments = []
-
-    for key in properties.keys():
-        arguments.append(key)
-        arguments.append(properties[key])
-
-    return arguments
 
 '''
 print('superheated:')
@@ -76,7 +39,17 @@ states[6]['P'] = states[1]['P']
 states[3]['P'] = states[4]['P']
 states[2]['P'] = states[4]['P']
 
-states[3]['Q'] = 0
+
+
+complete_state(states[2], fluid)
+
+print('state1:')
+print(states[1])
+print('state2:')
+print(states[2])
+print('state4:')
+print(states[4])
+
 
 
 #enthalpy_vaporizer = states[4]['H'] - states[3]['H']
